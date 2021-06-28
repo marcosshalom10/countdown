d1a1d13297769ce395246d11cf3bb84ff06492ed
marcosshalom10 <61480451+marcosshalom10@users.noreply.github.com>
2021-06-27 22:47:05 -0500

    Add files via upload

---
+++ countdown/main.js
@@ -0,0 +1,25 @@
+var countDate = new Date('May 5, 2020 00:00:00').getTime();
+
+function newCourse(){
+    var now = new Date().getTime();
+    var gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / second);
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    newCourse();
+},1000);
